b37965c198ad2498041c2cfa9bda24e90defc526
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-02-27 01:51:12 +0100

    just a simple change in SingleComment component

--- src/SingleComment.jsx
+++ src/SingleComment.jsx
@@ -4,7 +4,7 @@ const SingleComment = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={props.picture} alt="profile picture"></img>
+        <img src={props.picture} alt="profile"></img>
       </a>
       <div className="content">
         <a href="/" className="author">
