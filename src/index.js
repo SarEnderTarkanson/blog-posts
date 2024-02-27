25007dec3d2e37f14319faaa7c258b41621e129e
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-02-27 01:02:23 +0100

    Created another reusable component

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import profile1 from "./image/Profile_Picture.png";
 import profile2 from './image/profilePic.jpg'
 import profile3 from './image/profilePic2.jpg'
 import SingleComment from "./SingleComment";
+import UserCard from "./UserCard";
 
 
 const App = () => {
   return (
     <div className="ui comments">
-      <SingleComment name='Alpy' date='Today at 5 PM' text='Good job!' picture={profile1}/>
-      <SingleComment name='Sarah' date='Today at 6 PM' text='It is amazing' picture={profile2}/>
-      <SingleComment name='Alex' date='Today at 7 PM' text='Thank you' picture={profile3}/>
+      <UserCard><div>Hello. My name is Endo and I live in Steinkjer</div></UserCard>
+      <UserCard>
+        <SingleComment name='Alpy' date='Today at 5 PM' text='Good job!' picture={profile1} /></UserCard>
+      <UserCard><SingleComment name='Sarah' date='Today at 6 PM' text='It is amazing' picture={profile2} /></UserCard>
+      <UserCard><SingleComment name='Alex' date='Today at 7 PM' text='Thank you' picture={profile3} /></UserCard>
     </div>
   );
 };
